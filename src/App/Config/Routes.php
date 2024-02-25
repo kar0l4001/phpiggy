6764bcbfe5b411a7739d4c304150e49633478986
karol <karol4001@gmail.com>
2024-02-25 11:47:10 +0100

    working application after form validation chapter

---
+++ src/App/Config/Routes.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Config;
+
+use App\Controllers\{HomeController, AboutController, AuthController};
+use Framework\App;
+
+function registerRoutes(App $app)
+{
+    $app->get('/', [HomeController::class, 'home']);
+    // $app->get('/public/index.php', [HomeController::class, 'home']);
+    $app->get('/about', [AboutController::class, 'about']);
+    $app->get('/register', [AuthController::class, 'registerView']);
+    $app->post('/register', [AuthController::class, 'register']);
+}
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,10 +2,28 @@
 
 declare(strict_types=1);
 
+/*
+include - load file and gives warning if missing
+require - load file and throw error if missing
+require_once / include_once - not load file if it was already loaded
+*/
+
 require __DIR__ . "/../../vendor/autoload.php";
 
-use Framework\App;
+# TODO composer nie ładuje tych plików chociarz ma je wskazane w files, bez dodaktowego importu są niewidoczne
+include __DIR__ . "/Config/Middleware.php";
+include __DIR__ . "/Config/Routes.php";
 
-$app = new App();
+use function App\Config\registerMiddleware;
+use function App\Config\registerRoutes;
+use App\Config\Paths;
+use Framework\App;
+use App\Controllers\{HomeController, AboutController};
+
+
+$app = new App(Paths::SOURCE . "App/container-definitions.php");
+
+registerRoutes($app);
+registerMiddleware($app);
 
 return $app;
